6c0faafc05fc44086d4e2f48b5b6b638689289bd
Fauzi <fauzibinfaisal@gmail.com>
2026-06-08 22:26:00 +0700

    Initial Commit

---
+++ PersonalTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PersonalTracker
+//
+//  Created by fauzi on 08/06/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
